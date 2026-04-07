5c3d7c9d0b1e47bd51cc16deac6fedc9355cfe08
A-Karamazov <kabecudinho@gmail.com>
2026-04-07 13:25:13 -0300

    Refactor Triangulo class with improved validation

--- triangulo.ts
+++ triangulo.ts
@@ -1,31 +1,55 @@
+export { }
+
 class Triangulo {
-    lado1: number = 0;
-    lado2: number = 0;
-    lado3: number = 0;
+  lado1: number;
+  lado2: number;
+  lado3: number;
 
-    constructor(lado1: number, lado2: number, lado3: number) {
-        if (this.lado1 + this.lado2 < this.lado3 || this.lado1 + this.lado3 < this.lado2 || this.lado2 + this.lado3 < this.lado1) {
-            
-
-            }
-        this.lado1 = lado1;
-        this.lado2 = lado2;
-        this.lado3 = lado3;
-        
+  constructor(pLado1: number, pLado2: number, pLado3: number) {
+    if (pLado1 <= 0 || pLado2 <= 0 || pLado3 <= 0)
+       (pLado1 + pLado2 < pLado3 || pLado1 + pLado3 < pLado2 || pLado2 + pLado3 < pLado1) 
+    {
+      throw new Error("O valores não formam um triângulo válido");
     }
+    this.lado1 = pLado1;
+    this.lado2 = pLado2;
+    this.lado3 = pLado3;
+  }
 
-
-    tipo(lado1: number, lado2: number, lado3:  number, tipoTriangulo: string): string {
-        if (lado1 == lado2 && lado1 == lado3 && lado2 == lado3) {
-            return tipoTriangulo = "Equilátero";
-        }
-         if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
-            return tipoTriangulo = "Isósceles";
-        } 
-            return tipoTriangulo = "Escaleno";
+  get tipo(): string {
+    if (this.lado1 === this.lado2 && this.lado2 === this.lado3) {
+      return "equilátero";
     }
-
-    calcularPerimetro(lado1: number, lado2: number, lado3: number): number {
-         return (this.lado1 + this.lado2 + this.lado3) / 2;
+    if (this.lado1 === this.lado2 || this.lado2 === this.lado3 || this.lado1 === this.lado3) {
+      return "isósceles";
     }
+    return "escaleno";
+  }
+
+  get perimetro(): number {
+    return (this.lado1 + this.lado2 + this.lado3) / 2;
+  }
+
+  get area(): number {
+    const p = this.perimetro;
+    const a = this.lado1;
+    const b = this.lado2;
+    const c = this.lado3;
+    const area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    return area;
+  }
 }
+
+/*
+const tri1 = new Triangulo(10, 10, 10);
+const tri2 = new Triangulo(10, 10, 5);
+const tri3 = new Triangulo(10, 5, 6);
+
+tri1.lado1 = 20;
+tri1.lado2 = 20;
+tri1.lado3 = 20;
+
+console.log(tri1.tipo, tri1.perimetro);
+console.log(tri2.tipo, tri2.perimetro);
+console.log(tri3.tipo, tri3.perimetro); 
+*/ 
